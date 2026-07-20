f05a47c1cc3c38b481c955092b39a73ab864505b
saayi1998 <ananthasaayisaayi@gmail.com>
2026-07-20 10:44:29 +0530

    oop

--- Day 04 (17-7-26)/Task 09 - default Constructor Method/MainStudent.java
+++
@@ -1,9 +0,0 @@
-class MainStudent{
-	public static void main(String args[]){
-		
-		Student S; 
-		S = new Student(); 
-		
-		S.getStudent();
-	} 
-} 
--- Day 04 (17-7-26)/Task 10 - parameter Constructor Method/MainStudent.java
+++
@@ -1,9 +0,0 @@
-class MainStudent{
-	public static void main(String args[]){
-		
-		Student S; 
-		S = new Student(1,"Sai"); 
-		
-		S.getStudent();
-	} 
-} 
